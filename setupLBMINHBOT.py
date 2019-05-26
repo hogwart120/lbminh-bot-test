42d6bb5ab2281c57f2cfe94ff5ea6737e62d5b67
thangnd85 <thangnd85@gmail.com>
2019-05-26 22:18:22 +0700

    Fix setup

--- setupLBMINHBOT.py
+++ setupLBMINHBOT.py
@@ -1,12 +1,12 @@
 import os
+import time
 i = input('Bạn muốn cài đặt vào hệ thống (1) hay venv (2): ')
 if i == '1':
 	import st
 elif i == '2':
 	os.system('sudo apt-get install python3-dev python3-venv')
-	os.system('python3 -m venv env')
-	os.system('env/bin/python -m pip install --upgrade pip setuptools --upgrade')
-	os.system('source env/bin/activate')
+
+#	os.system('env/bin/python -m pip install --upgrade pip')
 	import st_env
 else:
 	print ('Bạn chọn sai rồi. Bye!')
