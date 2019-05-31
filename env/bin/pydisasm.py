07fdde56a7e09a3540d50cb769312454239987e0
thangnd85 <thangnd85@gmail.com>
2019-06-01 01:21:06 +0700

    Update env

---
+++ env/bin/pydisasm.py
@@ -0,0 +1,4 @@
+#!/home/pi/lbminh-bot/env/bin/python
+# EASY-INSTALL-SCRIPT: 'xdis==4.0.1','pydisasm.py'
+__requires__ = 'xdis==4.0.1'
+__import__('pkg_resources').run_script('xdis==4.0.1', 'pydisasm.py')
